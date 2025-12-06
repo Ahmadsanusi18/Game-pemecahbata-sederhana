b6240c9f51c5c2fb50abe02de5bead98849ef113
a.saan_ <ahmadsanusiii18@gmail.com>
2025-12-06 15:39:25 +0700

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -120,7 +120,7 @@ export default function TabLayout() {
                 options={{
                     title: 'Info',
                     // Ikon Baru: help-center (Pusat Bantuan)
-                    tabBarIcon: ({ color }) => <IconWrapper name="help-center" color={color} />,
+                    tabBarIcon: ({ color }) => <IconWrapper name="info" color={color} />,
                 }}
             />
         </Tabs>
